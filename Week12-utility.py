842ecedaef257402aa7a725b8dbe60ca4dc5f079
Hari <hniroula@mymai.mines.edu>
2019-11-21 19:54:26 -0700

    This is my second function

--- Week12-utility.py
+++ Week12-utility.py
@@ -8,3 +8,15 @@
 def printOutput(string):
     print('OUTPUT' , string)
 #printOutput('Hello World')
+
+
+####Function 2 [LOADFILE]
+def Loadfile(somefile):
+    my_list=[]
+    f= open(somefile)
+    lines=f.readlines()
+    for ln in lines:
+        my_list.append(ln[0:len(ln)-1])
+    return my_list
+#printOutput(Loadfile('test.txt') )       # I have file test as text saved in same loaction.
+
