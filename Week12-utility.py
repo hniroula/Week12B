6cecaa2d183402b73cb92f5bc373a62501762fd0
Hari <hniroula@mymai.mines.edu>
2019-11-21 19:50:34 -0700

    This is my first function

---
+++ Week12-utility.py
@@ -0,0 +1,10 @@
+
+#   Incremental build model
+#   Hari Niroula
+#  ​CSCI 102 – Section B 
+#   Week 12 - Part B 
+
+####Function 1 [PRINTOUTPUT]
+def printOutput(string):
+    print('OUTPUT' , string)
+#printOutput('Hello World')
